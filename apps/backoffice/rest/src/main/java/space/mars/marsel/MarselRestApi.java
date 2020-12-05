1295493cec08043a7617ab4567b194172916efd2
Borja Paz Rodríguez <borjapazr@gmail.com>
2020-12-06 00:52:04 +0100

    💄 Add .editorconfig

--- apps/backoffice/rest/src/main/java/space/mars/marsel/MarselRestApi.java
+++ apps/backoffice/rest/src/main/java/space/mars/marsel/MarselRestApi.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MarselRestApi {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarselRestApi.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MarselRestApi.class, args);
+    }
 
 }
